781158dc76a62d282fa9cee30e3bf6951da5e20c
Aidan O'Donnell <aidanodonnell9@gmail.com>
2023-06-04 19:39:00 -0400

    General Navigation bar layout w/o css

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import './navBar.css'
+//import './navBar.css'
 
 
 
@@ -8,6 +8,7 @@ function Navbar() {
     const [click, setClick] = useState(false);
 
     const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
     return (
         <>
             <nav className='navbar'>
@@ -19,6 +20,21 @@ function Navbar() {
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
                     <ul className={click ? 'nav-menu active' : 'nav-menu'}></ul>
+                    <li className='nav-item'>
+                        <Link to='/' className='navbarlinks' onClick={closeMobileMenu}>
+                            About Us
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/patient-forms' className='navbarlinks' onClick={closeMobileMenu}>
+                            Patient Forms
+                        </Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link to='/location' className='navbarlinks' onClick={closeMobileMenu}>
+                            Location
+                        </Link>
+                    </li>
                 </div>
             </nav>
 
